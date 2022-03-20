feaa64e4b80176573cb124432bdb8888639d7e21
Pavel Kuprianov <97345976+PavelKuprianov@users.noreply.github.com>
2022-03-20 23:01:28 +0300

    20.03.2022

---
+++ src/Card/Card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import '../App.css';
+
+export const Card = () => (
+    <div className="Card">
+      Card
+    </div>
+)
+
+
---
+++ src/Input/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import '../App.css';
+
+export const Input = () => (
+    <div className="InputWrap">
+      <input className="Input" />
+      <button className="Button">+</button>
+    </div>
+)
+
+
